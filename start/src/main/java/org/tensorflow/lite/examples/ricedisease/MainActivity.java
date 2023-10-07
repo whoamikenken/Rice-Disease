1bee8b23e446ae972c425a33d953c8039228e739
whoamiken <whoamikenken@gmail.com>
2023-10-07 16:28:35 +0800

    Initial commit

---
+++ start/src/main/java/org/tensorflow/lite/examples/ricedisease/MainActivity.java
@@ -0,0 +1,61 @@
+package org.tensorflow.lite.examples.ricedisease;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.os.Handler;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static  int SPLASH_SCREEN = 4000;
+    Animation animate;
+    ImageView splashImg;
+
+    SharedPreferences sharedP;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Hide ActionBar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        //Splash Screen
+        animate = AnimationUtils.loadAnimation(this,R.anim.animate);
+        splashImg = findViewById(R.id.splashImg);
+
+        splashImg.setAnimation(animate);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //Show onBoarding screen only to new user
+                sharedP = getSharedPreferences("Onboarding", MODE_PRIVATE);
+                boolean firstimer = sharedP.getBoolean("firstimer", true);
+
+                if(firstimer){
+                    SharedPreferences.Editor edit = sharedP.edit();
+                    edit.putBoolean("firstimer", false);
+                    edit.apply();
+
+                    Intent onboard = new Intent(MainActivity.this, Onboarding.class);
+                    startActivity(onboard);
+                    finish();
+                }
+                else{
+                    Intent gotohome = new Intent(MainActivity.this, Click_Main.class);
+                    startActivity(gotohome);
+                    finish();
+                }
+            }
+        }, SPLASH_SCREEN);
+
+    }
+}
